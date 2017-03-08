40deac277040d9698625b5ee14234eb8b815bbcb
ACER <ACER@192.168.1.19>
2017-03-08 14:25:08 -0300

    Voltando a trabalhar no master

--- ProjetoVersionado/src/br/univel/Principal.java
+++ ProjetoVersionado/src/br/univel/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 		System.out.println("Mundo, fale comigo!");
 		System.out.println("I dont wanna talk to you anyway...");
 		System.out.println("Hello darkness my old friend...");
+		System.out.println("I've come to talk with you again.");
 	}
 
 }
